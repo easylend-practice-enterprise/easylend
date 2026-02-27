b62c057d0567d4e71804393061499b45433cd0b7
Jasper Savels <73786910+Jaskejaske1@users.noreply.github.com>
2026-02-28 00:01:46 +0100

    Feat/elp 19 sqlalchemy schemas (#18)
    * feat(db): draft sqlalchemy models and configure timezone-aware datetimes
    
    * feat(db): ELP-19 setup sqlalchemy schemas, async engine, migrations and documentation

--- backend/api/app/core/config.py
+++ backend/api/app/core/config.py
@@ -5,9 +5,7 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = "EasyLend API"
     # Als er geen .env is, valt hij terug op je lokale docker-compose instellingen
     REDIS_URL: str = "redis://:redis_geheim_456@localhost:6379/0"
-    DATABASE_URL: str = (
-        "postgresql+asyncpg://easylend_user:super_geheim_123@localhost:5432/easylend_db"
-    )
+    DATABASE_URL: str = "postgresql+asyncpg://easylend:supergeheim_wachtwoord_123@localhost:5432/easylend_db"
 
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="ignore"
