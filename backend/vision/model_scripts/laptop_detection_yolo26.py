4fa4c2eda738301fecba0a9066a7047621f90b46
injodp <114516288+injodp@users.noreply.github.com>
2026-03-18 18:48:29 +0100

    updated ai model scripts (#45)

--- backend/vision/model_scripts/laptop_detection_yolo26.py
+++ backend/vision/model_scripts/laptop_detection_yolo26.py
@@ -6,13 +6,13 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 def main():
-    model = YOLO("yolo26m-seg.pt")
+    model = YOLO("yolo26m.pt")
     model.train(
-        data="datasets/Laptop.v2-laptop.yolo26/data.yaml",
-        epochs=10,
+        data="datasets/laptop_detection_YOLO26/data.yaml",
+        epochs=100,
         imgsz=640,
         device=0,
-        name="yolo26m-seg_laptop_detection",
+        name="yolo26m-laptop_detection",
         project=os.path.join(current_dir, "runs", "laptop-detection"),
     )
 
